7a2ab41c0262dd379da64b6e9828c645734db963
sia20181011 <sia20181011@duksung.ac.kr>
2021-07-15 23:50:23 +0900

    index수정

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const bodyParser = require('body-parser');
+const {User} = require("./models/User");
+
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
 const mongoose = require('mongoose')
 
 mongoose.connect('mongodb+srv://sia20181011:angel6841!@bolierplate.bxwrm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
@@ -12,6 +21,22 @@ mongoose.connect('mongodb+srv://sia20181011:angel6841!@bolierplate.bxwrm.mongodb
 app.get('/', (req, res) => {
   res.send('Hello World!~~~~~안녕하세요')
 })
+
+
+app.post('/register', (req, res) => {
+  //회원가입 할때 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다.
+
+
+  const user = new User(req.body)
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
